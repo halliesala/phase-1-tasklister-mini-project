69a80da6ab27fa3a85ab2fddaf2d0d2e7487f23f
Hallie Sala <halliesala19@gmail.com>
2023-07-12 16:17:17 -0400

    first pass at tasklister lab

--- src/index.js
+++ src/index.js
@@ -4,23 +4,12 @@ document.addEventListener("DOMContentLoaded", () => {
   const inputForm = document.querySelector('#create-task-form');
   const taskList = document.querySelector('#list');
   
-  // I'm going to try to replace the ul with a form so I can have check boxes
-  const listForm = document.createElement('form');
-  listForm.input
 
   // Adds tasks to unordered list
   inputForm.addEventListener('submit', (e) => {
     e.preventDefault();
     const userSubmittedText = e.target.newTaskDescription.value;
-    const newTask = document.createElement('p');
-
-    // I'd like to move the 'delete' functionality to a button 
-    // And append the button to the list item
-
-    const newDeleteButton = document.createElement('button');
-    newDeleteButton.innerText = "I'm a button!";
-    newTask.appendChild(newDeleteButton);
-
+    const newTask = document.createElement('li');
     newTask.onclick = () => newTask.remove();
     newTask.textContent = userSubmittedText;
     taskList.appendChild(newTask);
