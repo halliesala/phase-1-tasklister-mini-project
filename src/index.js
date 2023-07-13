30b6e50354d6f6b1e390b1d3faa46cb275d9921b
Hallie Sala <halliesala19@gmail.com>
2023-07-13 13:48:03 -0400

    Added checkboxes

--- src/index.js
+++ src/index.js
@@ -1,21 +1,44 @@
-// What is this frame function?
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-  const inputForm = document.querySelector('#create-task-form');
-  const taskList = document.querySelector('#list');
-  
 
-  // Adds tasks to unordered list
-  inputForm.addEventListener('submit', (e) => {
-    e.preventDefault();
-    const userSubmittedText = e.target.newTaskDescription.value;
-    const newTask = document.createElement('li');
-    newTask.onclick = () => newTask.remove();
-    newTask.textContent = userSubmittedText;
-    taskList.appendChild(newTask);
-    e.target.newTaskDescription.value = '';
-  });
+const inputForm = document.querySelector('#create-task-form');
+const taskList = document.querySelector('#tasks');
+
+// Adds tasks to list
+inputForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const userSubmittedText = e.target.newTaskDescription.value;
+
+  // Each task is created as a form inside a list item
+  const newLi = document.createElement('li');
+
+  const newForm = document.createElement('form');
+
+  const newFormLabel = document.createElement('label');
+  newFormLabel.textContent = userSubmittedText + ' ';
+
+  const newFormInput = document.createElement('input');
+  newFormInput.setAttribute('type', 'checkbox');
+
+  const newFormSubmit = document.createElement('submit');
+  newFormSubmit.textContent = ' ❌ ';
+  deleteOnClick(newLi, newFormSubmit);
+
+  newForm.appendChild(newFormLabel);
+  newForm.appendChild(newFormInput);
+  newForm.appendChild(newFormSubmit);
+
+  newLi.appendChild(newForm);
+  taskList.appendChild(newLi);
+
+  // Clear the form
+  e.target.newTaskDescription.value = '';
 });
+
+
+
+// Delete target when trigger clicked
+function deleteOnClick(target, trigger) {
+  trigger.addEventListener('click', () => target.remove());
+}
 
 
 
